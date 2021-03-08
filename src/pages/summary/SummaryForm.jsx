5bff275c54acba381c7e1033b2077ec1eeebee50
Brad <brad.rivenburgh@gmail.com>
2021-03-08 16:42:58 -0500

    modified tests for popover and implemented popover functionality in SummaryForm

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 function SummaryForm() {
-  const [enable, setEnable] = useState(false);
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Content>No ice cream will actually be delivered</Popover.Content>
+    </Popover>
+  );
+
+  const checkboxLabel = (
+    <span>
+      I agree to 
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
+  );
+
   return (
-    <div>
-      <button type="submit" aria-disabled={!enable} disabled={!enable}>
-        Submit
-      </button>
-      <input
-        type="checkbox"
-        id="agree"
-        checked={enable}
-        onChange={() => setEnable(!enable)}
-      />
-      <label htmlFor="agree">Agree</label>
-    </div>
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
   );
 }
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import UserEvent from "@testing-library/user-event";
 
 describe("SummaryForm", () => {
   test("renders without crashing", () => {
@@ -8,7 +9,9 @@ describe("SummaryForm", () => {
 
   test("checkbox is unchecked and button is disabled by default", () => {
     render(<SummaryForm />);
-    const checkbox = screen.getByRole("checkbox", { name: /terms and conditions/i });
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
     const button = screen.getByRole("button", { name: /confirm order/i });
 
     // Checkbox should be unchecked and button disabled
@@ -18,13 +21,41 @@ describe("SummaryForm", () => {
 
   test("checking checkbox enables button, unchecking disables it", () => {
     render(<SummaryForm />);
-    const checkbox = screen.getByRole("checkbox", { name: /terms and conditions/i });
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
     const button = screen.getByRole("button", { name: /confirm order/i });
 
-    fireEvent.click(checkbox);
+    UserEvent.click(checkbox);
     expect(button).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    UserEvent.click(checkbox);
     expect(button).toBeDisabled();
+  });
+
+  test("popover responds to hover", async () => {
+    render(<SummaryForm />);
+
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears upon mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    UserEvent.hover(termsAndConditions);
+
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    // popover dissapears when we mouse out
+    UserEvent.unhover(termsAndConditions);
+
+    await waitForElementToBeRemoved(() => screen.queryByText(
+      /no ice cream will actually be delivered/i
+    ));
   });
 });
