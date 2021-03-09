02ab204df2574c4e33583e4664902e24b634fb6a
Brad <brad.rivenburgh@gmail.com>
2021-03-09 17:22:05 -0500

    Code and test scoop option images from server

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,5 +1,22 @@
-function ScoopOption() {
-  return <div></div>
+import Col from 'react-bootstrap/Col';
+
+function ScoopOption({ name, imagePath }) {
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+      <div>
+        <img
+          style={{ width: '75%' }}
+          src={`http://localhost:3030/${imagePath}`}
+          alt={`${name} scoop`}
+        />
+      </div>
+    </Col>
+  );
 }
+
+ScoopOption.defaultProps = {
+  name: "Vanilla scoop",
+  imagePath: "http://localhost:3030/chocolate",
+};
 
 export default ScoopOption;
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,20 +2,15 @@ import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
 describe("Options", () => {
-  test("renders without crashing", () => {
-    // Not passing optionType prop to test defaultProps
-    render(<Options />);
-  });
-
-  test("displays image for each scoop from server", () => {
+  test("displays image for each scoop from server", async () => {
     render(<Options optionType="scoops" />);
 
     // find the images
-    const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+    const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
     expect(scoopImages).toHaveLength(2);
 
     // confirm alt text of images
     const altText = scoopImages.map((element) => element.alt);
-    expect(altText).toEqual(['Chocolate scoop', "Vanilla scoop"])
+    expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
   });
 });
