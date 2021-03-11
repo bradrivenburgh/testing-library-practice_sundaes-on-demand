d2050384b922aa50c7aa442cfc8bdd4eddcd5277
Brad <brad.rivenburgh@gmail.com>
2021-03-11 14:21:03 -0500

    added error handling, common directory with AlertBanner, and tests / updates to the entry components

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -1,5 +1,12 @@
+import Options from "./Options";
+
 function OrderEntry() {
-  return <div></div>
+  return (
+    <div>
+      <Options optionType="scoops" />
+      <Options optionType="toppings" />
+    </div>
+  );
 }
 
 export default OrderEntry;
