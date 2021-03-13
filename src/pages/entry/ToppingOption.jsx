524e4a74ffc6cf2c67acacea282f5bc4779d7348
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-03-13 13:09:47 -0500

    wrote test for toppings subtotal

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,8 @@
 import Col from "react-bootstrap/Col";
 
 function ToppingOption({ name, imagePath }) {
+  
+  
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <div>
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,9 +1,13 @@
-import { render, screen } from "../../../test-utils/testing-library-utils";
+import {
+  findAllByRole,
+  render,
+  screen,
+} from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
 
 test("update scoop subtotal when scoops change", async () => {
-  render(<Options optionType="scoops" />);
+  render(<Options optionType='scoops' />);
 
   // make sure total starts out at $0.00
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
@@ -24,4 +28,30 @@ test("update scoop subtotal when scoops change", async () => {
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+test("update toppings subtotal when toppings change", async () => {
+  render(<Options optionType='toppings' />);
+
+  // make sure toppings subtotal starts out at $0.00
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  // make sure toppings options are unchecked
+  const toppingsInputs = await screen.findAllByRole("checkbox");
+  toppingsInputs.forEach((element) => expect(element).not.toBeChecked());
+
+  // tick one checkbox and assert on subtotal
+  userEvent.click(toppingsInputs[0]);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  // tick another checkbox and assert on subtotal
+  userEvent.click(toppingsInputs[1]);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  // uncheck a checkbox and assert on the subtotal
+  userEvent.click(toppingsInputs[0]);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
 });
